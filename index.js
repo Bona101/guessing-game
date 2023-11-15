06d7ad395e82ecfe4c3f804bc022ff3d03726ef9
Bona101 <bonaventurembbahoffor@gmail.com>
2023-11-15 19:59:29 +0100

    Multiple attempts

--- index.js
+++ index.js
@@ -5,16 +5,36 @@ const lose = document.querySelector(".lose");
 const check = document.querySelector(".check");
 const displayAnswer = document.querySelector(".display-answer");
 
+let answer;
+let round = 0;
+
 check.addEventListener("click", () => {
-    let answer = Math.floor(Math.random() * 10) + 1;
+    if (round === 0){
+        answer = Math.floor(Math.random() * 10) + 1;
+    }
     let guess = text.value;
     if (answer == guess){
         lose.classList.add("none");
         win.classList.remove("none");
+        displayAnswer.textContent = "";
+        round = 0;
     } else {
         win.classList.add("none");
-        lose.classList.remove("none");
-        displayAnswer.textContent = `Answer was ${answer}`;
+        if (+guess > answer){
+            displayAnswer.textContent = `Too High. ${round + 1}/3 attempts`;
+        } else {
+            displayAnswer.textContent = `Too low. ${round + 1}/3 attempts`;
+        }
+
+        
+        if (round === 2){
+            lose.classList.remove("none");
+            displayAnswer.textContent = `Answer was ${answer}`;
+            round = -1;
+        } 
+        
+
+        round++;
     }
 
 })
